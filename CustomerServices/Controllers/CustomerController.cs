e5ff42209a9bf27061948b955e1dac2a322d8a9f
Blake14 <blakemcpherson2@gmail.com>
2021-07-17 01:13:14 -0500

    Add project files.

---
+++ CustomerServices/Controllers/CustomerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CustomerDataAccessv2;
+using CustomerServices;
+
+namespace CustomerServices.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        public IEnumerable<customer_data> Get()
+        {
+            using (CustomersEntities entities = new CustomersEntities())
+            {
+                return entities.customer_data.ToList();
+            }
+        }
+
+        public customer_data Get(int id)
+        {
+            using(CustomersEntities entities = new CustomersEntities())
+            {
+                return entities.customer_data.FirstOrDefault(e => e.id == id);
+            }
+        }
+    }
+}
